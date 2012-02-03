038b5daa8aa1e5e582ec7f83122f424b44b470be
David Reuss <david@upsys.dk>
2012-02-03 16:26:59 +0100

    initial work on 'connected', and 'giant status'

--- giantbook/GiantBook.java
+++ giantbook/GiantBook.java
@@ -1,10 +1,33 @@
 public class GiantBook {
+
+	private WeightedQuickUnionUF wqf;
+	private static boolean reachedGiantStatus = false;
 	
-	private int count;
+	private static int generateRandomNumber(int hi, int low) {
+		int random = StdRandom.uniform(hi, low);		
+		// StdOut.println("Generated random number: " + random);
+		return random;
+	}
 
 	public static void main(String[] args) {
 		int count = StdIn.readInt();
-		StdOut.println("I am a giant book, with " + count + " elements");
+		WeightedQuickUnionUF wqf = new WeightedQuickUnionUF(count);
+		
+		while (wqf.count() != 1) {
+			wqf.union(
+				generateRandomNumber(0, count),
+				generateRandomNumber(0, count)
+			);
+			
+			StdOut.print(".");
+
+			if (!reachedGiantStatus && wqf.isGiantComponentReached()) {
+				reachedGiantStatus = true;
+				StdOut.print("G");
+			}
+		}
+
+		StdOut.print("C\n");
 	}
 
 }
