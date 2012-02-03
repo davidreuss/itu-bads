3d178d0fb8a6cf9422f0101ce335bc9094e7b6c4
David Reuss <david@upsys.dk>
2012-02-03 16:31:12 +0100

    remove unnecessary wrapper for random generation

--- giantbook/GiantBook.java
+++ giantbook/GiantBook.java
@@ -3,20 +3,14 @@ public class GiantBook {
     private WeightedQuickUnionUF wqf;
     private static boolean reachedGiantStatus = false;
 
-    private static int generateRandomNumber(int hi, int low) {
-        int random = StdRandom.uniform(hi, low);		
-        // StdOut.println("Generated random number: " + random);
-        return random;
-    }
-
     public static void main(String[] args) {
         int count = StdIn.readInt();
         WeightedQuickUnionUF wqf = new WeightedQuickUnionUF(count);
 
         while (wqf.count() != 1) {
             wqf.union(
-                generateRandomNumber(0, count),
-                generateRandomNumber(0, count)
+                StdRandom.uniform(count),
+                StdRandom.uniform(count)
             );
 
             StdOut.print(".");
