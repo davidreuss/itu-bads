78a9c2ab300d9330cec3cac16513652513ca2480
David Reuss <david@upsys.dk>
2012-02-03 13:44:13 +0100

    Added weighted quick union

--- giantbook/GiantBook.java
+++ giantbook/GiantBook.java
@@ -1,7 +1,10 @@
 public class GiantBook {
+	
+	private int count;
 
 	public static void main(String[] args) {
-		StdOut.println("I am a giant book!");
+		int count = StdIn.readInt();
+		StdOut.println("I am a giant book, with " + count + " elements");
 	}
 
 }
